d61ed245be86b02c7946de40c3fefdb9d991ffad
lolivai <103974102+lolivai@users.noreply.github.com>
2022-06-09 17:16:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 from JDatabase import JsonDatabase
 import start
 
-permitidos = ["Hiyabo"]
+permitidos = ["andres950108"]
 
 def filtrar_text(update, context):
     text = update.message.text
