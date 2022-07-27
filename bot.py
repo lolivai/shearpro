79638644ee103031371c29efffaad4efbfe5333f
lolivai <103974102+lolivai@users.noreply.github.com>
2022-07-27 06:29:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def filtrar_text(update, context):
                         jdb.propietario(username)
                     user_info = jdb.get_user(username)
                     jdb.save()    
-           else:return
+ else:return
             
   
         
